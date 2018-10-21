442e9c5ac6be4591a52feddecee247a5c59bcb81
hugmeonce <grazingpenguin@gmail.com>
2018-10-20 20:47:51 -0700

    Add dungeon sounds

--- BearForceOne.lua
+++ BearForceOne.lua
@@ -19,6 +19,11 @@ local ACHIEVEMENT_SOUND = SOUNDS_DIR .. "achievement.ogg"
 local NEW_PARTY_MEMBER_SOUND = SOUNDS_DIR .. "party join.ogg"
 local PLAYER_DEATH_SOUND = SOUNDS_DIR .. "death.ogg"
 
+-- Dungeon
+local QUEUE_SOUND = SOUNDS_DIR .. "pushbutton.ogg"
+local KILL_BOSS_SOUND = SOUNDS_DIR .. "pushbutton.ogg"
+local WIPE_SOUND = SOUNDS_DIR .. "pushbutton.ogg"
+
 local PVP_KILLS_SOUNDS = {
   SOUNDS_DIR .. "achievement.ogg",
   SOUNDS_DIR .. "death.ogg",
@@ -26,6 +31,14 @@ local PVP_KILLS_SOUNDS = {
   SOUNDS_DIR .. "honor kill.ogg",
   SOUNDS_DIR .. "party join.ogg",
 }
+
+local NUM_DPS_SOUNDS = 7
+local NUM_ENCOUNTER_SOUNDS = 7
+local NUM_HEALER_SOUNDS = 6
+local NUM_INTERRUPT_SOUNDS = 8
+local NUM_TANK_SOUNDS = 8
+local NUM_VICTORY_SOUNDS = 7
+local NUM_WIPE_SOUNDS = 8
 
 local playingBFOMusic = false
 local lastPvpSoundTime = 0
@@ -61,6 +74,7 @@ local defaultConfig = {
   SOUND_ENABLED = true,
   ANIM_ENABLED = true,
   MGS_PERCENT = 0,
+  DUNGEON_SOUND_ENABLED = true,
 }
 
 -- Overriding DoEmote is probably a bad idea...
@@ -95,6 +109,12 @@ end
 
 function playBFOSound(soundFile)
   if bfoConfig.SOUND_ENABLED then
+    PlaySoundFile(soundFile)
+  end
+end
+
+function playDungeonSound(soundFile)
+  if bfoConfig.DUNGEON_SOUND_ENABLED then
     PlaySoundFile(soundFile)
   end
 end
@@ -250,11 +270,18 @@ function events:ENCOUNTER_START()
   if bfoConfig.MUSIC_ENABLED then
     PlayMusic(BOSS_MUSIC)
   end
+
+  playDungeonSound(SOUNDS_DIR .. "encounter" .. math.random(NUM_ENCOUNTER_SOUNDS) .. ".ogg")
 end
 
-function events:ENCOUNTER_END()
+function events:ENCOUNTER_END(encounterID, encounterName, difficultyID, groupSize, success)
   if bfoConfig.MUSIC_ENABLED then
     StopMusic()
+  end
+  if success == 1 then
+    playDungeonSound(SOUNDS_DIR .. "victory" .. math.random(NUM_VICTORY_SOUNDS) .. ".ogg")
+  else
+    playDungeonSound(SOUNDS_DIR .. "wipe" .. math.random(NUM_WIPE_SOUNDS) .. ".ogg")
   end
 end
 
@@ -280,10 +307,110 @@ function events:ZONE_CHANGED_NEW_AREA()
   end
 end
 
+function events:LFG_ROLE_CHECK_SHOW()
+  playDungeonSound(QUEUE_SOUND)
+end
+
 -- I should confirm that this actually does something
 function events:VARIABLES_LOADED(addonName)
   if not bfoConfig then
     bfoConfig = defaultConfig;
+  end
+
+  if not bfoConfig.DUNGEON_SOUND_ENABLED then
+    bfoConfig.DUNGEON_SOUND_ENABLED = defaultConfig.DUNGEON_SOUND_ENABLED
+  end
+end
+
+local dps_cooldowns = {
+  [275699] = true, -- Apocalypse (Death Knight)
+  [42650] = true, -- Army of the Dead (Death Knight)
+  [47568] = true, -- Empower Rune Weapon (Death Knight)
+  [187827] = true, -- Metamorphosis (Demon Hunter)
+  [106951] = true, -- Berserk (Druid)
+  [194223] = true, -- Celestial Alignment (Druid)
+  [186289] = true, -- Aspect of the Eagle (Hunter)
+  [19574] = true, -- Bestial Wrath (Hunter)
+  [231548] = true, -- Bestial Wrath r2 (Hunter)
+  [266779] = true, -- Coordinated Assault (Hunter)
+  [193256] = true, -- Trueshot (Hunter)
+  [12042] = true, -- Arcane Power (Mage)
+  [190319] = true, -- Combustion (Mage)
+  [12472] = true, -- Icy Veins (Mage)
+  [190319] = true, -- Combustion (Mage)
+  [34433] = true, -- Shadowfiend (Priest)
+  [13750] = true, -- Adrenaline Rush (Rogue)
+  [121471] = true, -- Shadow Blades (Rogue)
+  [79140] = true, -- Vendetta (Rogue)
+  [205180] = true, -- Summon Darkglare (Warlock)
+  [265187] = true, -- Summon Demonic Tyrant (Warlock)
+  [1122] = true, -- Summon Infernal (Warlock)
+  [227847] = true, -- Bladestorm (Warrior)
+  [1719] = true, -- Recklessness (Warrior)
+}
+
+local healer_cooldowns = {
+  [29166] = true, -- Innervate (Druid)
+  [740] = true, -- Tranquility (Druid)
+  [116849] = true, -- Life Cocoon (Monk)
+  [115310] = true, -- Revival (Monk)
+  [31821] = true, -- Aura Mastery (Paladin)
+  [1022] = true, -- Blessing of Protection (Paladin)
+  [184662] = true, -- Shield of Vengeance (Paladin)
+  [64843] = true, -- Divine Hymn (Priest)
+  [47788] = true, -- Guardian Spirit (Priest)
+  [33206] = true, -- Pain Suppression (Priest)
+  [62618] = true, -- Power Word: Barrier (Priest)
+  [47536] = true, -- Rapture (Priest)
+  [64901] = true, -- Symbol of Hope (Priest)
+  [108280] = true, -- Healing Tide Totem (Shaman)
+  [98008] = true, -- Spirit Link Totem (Shaman)
+}
+
+local tank_cooldowns = {
+  [61336] = true, -- Survival Instincts (Druid)
+  [115203] = true, -- Fortifying Brew (Monk)
+  [115176] = true, -- Zen Meditation (Monk)
+  [31850] = true, -- Ardent Defender (Paladin)
+  [86659] = true, -- Guardian of Ancient Kings (Paladin)
+  [198103] = true, -- Earth Elemental (Shaman)
+  [118038] = true, -- Die by the Sword (Warrior)
+  [12975] = true, -- Last Stand (Warrior)
+  [97462] = true, -- Rallying Cry (Warrior)
+}
+
+local interrupts = {
+  [47528] = true, -- Mind Freeze (Death Knight)
+  [183752] = true, -- Disrupt (Demon Hunter)
+  [78675] = true, -- Solar Beam (Druid)
+  [106839] = true, -- Skull Bash (Druid)
+  [147362] = true, -- Counter Shot (Hunter)
+  [2139] = true, -- Counterspell (Mage)
+  [116705] = true, -- Spear Hand Strike (Monk)
+  [96231] = true, -- Rebuke (Paladin)
+  [15487] = true, -- Silence (Priest)
+  [1766] = true, -- Kick (Rogue)
+  [57994] = true, -- Wind Shear (Shaman)
+  [19647] = true, -- Spell Lock (Warlock)
+  [6552] = true, -- Pummel (Warrior)
+}
+
+function events:COMBAT_LOG_EVENT_UNFILTERED()
+  spellId = select(12, CombatLogGetCurrentEventInfo())
+  spellName = select(13, CombatLogGetCurrentEventInfo())
+  event = select(2, CombatLogGetCurrentEventInfo())
+
+  if dps_cooldowns[spellId] then
+    if event ~= "SPELL_AURA_APPLIED" then return end
+    playDungeonSound(SOUNDS_DIR .. "dps" .. math.random(NUM_DPS_SOUNDS) .. ".ogg")
+  elseif healer_cooldowns[spellId] then
+    if event ~= "SPELL_AURA_APPLIED" then return end
+    playDungeonSound(SOUNDS_DIR .. "healer" .. math.random(NUM_HEALER_SOUNDS) .. ".ogg")
+  elseif tank_cooldowns[spellId] then
+    if event ~= "SPELL_AURA_APPLIED" then return end
+    playDungeonSound(SOUNDS_DIR .. "tank" .. math.random(NUM_TANK_SOUNDS) .. ".ogg")
+  elseif interrupts[spellId] then
+    playDungeonSound(SOUNDS_DIR .. "interrupt" .. math.random(NUM_INTERRUPT_SOUNDS) .. ".ogg")
   end
 end
 
